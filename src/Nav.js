963cc3f8c2a76eacc4f59a7c4e31fa53cdd007c2
Sone <sthaya@gmail.com>
2020-07-22 19:12:03 -0700

    imported netflix logo and user logo to nav component

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f"
+        alt="netflix logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/12401199904155" alt="netflix logo" />
+    </div>
+  )
+}
+
+export default Nav
