2be8eaf4d717de2cfcf88b86abd940597fe56b58
Sone <sthaya@gmail.com>
2020-07-22 19:27:02 -0700

    styled nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './Nav.css'
 
 const Nav = () => {
+
+  const [show, handleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true)
+      } else handleShow(false);
+    })
+  }, [])
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0f"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
         alt="netflix logo"
       />
 
       <img
         className="nav__avatar"
-        src="https://pbs.twimg.com/profile_images/12401199904155" alt="netflix logo" />
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="netflix logo" />
     </div>
   )
 }
